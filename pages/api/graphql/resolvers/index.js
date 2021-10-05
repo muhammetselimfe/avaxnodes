df42ca6b5649176ea1e1cfaf4b3157f8b7be68c7
hiccup1991 <hiccup1991@hotmail.com>
2021-10-05 02:08:23 -0400

    detail pages of c-chain, fix some bugs

--- pages/api/graphql/resolvers/index.js
+++ pages/api/graphql/resolvers/index.js
@@ -2,8 +2,11 @@ import node from "./node";
 import nodes from "./nodes";
 import stats from "./stats";
 import notifierStats from "./notifierStats";
+import transaction from './transaction';
 import transactions from "./transactions";
+import block from './block';
 import blocks from "./blocks";
+import token from './token';
 import tokens from "./tokens";
 
 export const resolvers = {
@@ -12,8 +15,11 @@ export const resolvers = {
     nodes: nodes,
     node: node,
     notifierStats: notifierStats,
+    transaction:transaction,
     transactions:transactions,
+    block:block,
     blocks:blocks,
+    token:token,
     tokens:tokens,
   }
 };
